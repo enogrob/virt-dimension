28335eeb19f19336db43c0f820bbcf740ddf3928
Roberto Nogueira <enogrob@gmail.com>
2015-08-07 05:18:15 -0300

    First commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,6 @@
+﻿$name = 'virt-dimension'
+$installerType = 'exe'
+$url  = 'http://prdownloads.sourceforge.net/virt-dimension/VirtualDimension-0.94.exe?download'
+$silentArgs = '/S'
+
+Install-ChocolateyPackage $name $installerType $silentArgs $url
